885a8494e7d09dc1b452ee43c5259807bc1ce502
bakhromov <najottalim@gmail.com>
2024-08-06 21:30:51 +0500

    API comment

--- shop/views.py
+++ shop/views.py
@@ -1,14 +1,94 @@
-
-from rest_framework.response import Response
-from rest_framework.views import APIView
-
-from shop.models import Category
-
-
 # Create your views here.
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import generics, status
+from shop.models import Category, Product
+from shop.serializers import CategorySerializer, ProductSerializer
 
-class CategoryList(APIView):
 
-    def get(self, request):
-        categories = [category.title for category in Category.objects.all()]
-        return Response(categories)
+class CategoryListView(generics.ListAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class CategoryDetail(generics.RetrieveAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def retrieve(self, request, *args, **kwargs):
+        slug = self.kwargs['slug']
+        category = Category.objects.get(slug=slug)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, *args, **kwargs):
+        slug = self.kwargs['slug']
+        category = Category.objects.get(slug=slug)
+        serializer = CategorySerializer(category, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, *args, **kwargs):
+        slug = self.kwargs['slug']
+        category = Category.objects.get(slug=slug)
+        category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class CreateCategoryView(generics.CreateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class UpdateCategoryView(generics.UpdateAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+    def get(self, request, *args, **kwargs):
+        slug = self.kwargs['slug']
+        category = get_object_or_404(Category, slug=slug)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def put(self, request, *args, **kwargs):
+        slug = self.kwargs['slug']
+        category = get_object_or_404(Category, slug=slug)
+        serializer = CategorySerializer(category, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class DeleteCategoryView(generics.DestroyAPIView):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+    lookup_field = 'slug'
+
+    def get(self, request, *args, **kwargs):
+        slug = self.kwargs['slug']
+        category = get_object_or_404(Category, slug=slug)
+        serializer = CategorySerializer(category)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    #
+    def delete(self, request, *args, **kwargs):
+        slug = self.kwargs['slug']
+        category = get_object_or_404(Category, slug=slug)
+        category.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ProductListView(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    lookup_field = 'slug'
