5cf579c3dcad782eb8049c9bb5a1af9d1fc9ab7a
bakhromov <najottalim@gmail.com>
2024-08-06 21:38:00 +0500

    API comment

--- shop/urls.py
+++ shop/urls.py
@@ -1,8 +1,13 @@
-from django.contrib import admin
-from django.urls import path, include
-
-from shop.views import CategoryList
+from shop.views import CategoryListView, CategoryDetail, CreateCategoryView, UpdateCategoryView, DeleteCategoryView, ProductListView, ProductDetail
+from django.urls import path
 
 urlpatterns = [
-    path('category/', CategoryList.as_view(), name='category'),
+    path('categories/', CategoryListView.as_view(), name='category_list'),
+    path('category/<slug:slug>/', CategoryDetail.as_view(), name='category_detail'),
+    path('category-create/', CreateCategoryView.as_view(), name='category_create'),
+    path('category/<slug:slug>/update/', UpdateCategoryView.as_view(), name='category_update'),
+    path('category/<slug:slug>/delete/', DeleteCategoryView.as_view(), name='category_delete'),
+    path('products/', ProductListView.as_view(), name='product_list'),
+    path('products/<slug:slug>/', ProductDetail.as_view(), name='product_detail'),
+
 ]
