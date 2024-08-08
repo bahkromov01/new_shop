16c0959112967105ac05d978d077038c14cab856
bakhromov <najottalim@gmail.com>
2024-08-09 02:12:32 +0500

    Product list comment

--- shop/urls.py
+++ shop/urls.py
@@ -1,13 +1,14 @@
-from shop.views import CategoryListView, CategoryDetail, CreateCategoryView, UpdateCategoryView, DeleteCategoryView, ProductListView, ProductDetail
+from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('categories/', CategoryListView.as_view(), name='category_list'),
-    path('category/<slug:slug>/', CategoryDetail.as_view(), name='category_detail'),
-    path('category-create/', CreateCategoryView.as_view(), name='category_create'),
-    path('category/<slug:slug>/update/', UpdateCategoryView.as_view(), name='category_update'),
-    path('category/<slug:slug>/delete/', DeleteCategoryView.as_view(), name='category_delete'),
-    path('products/', ProductListView.as_view(), name='product_list'),
-    path('products/<slug:slug>/', ProductDetail.as_view(), name='product_detail'),
+    path('categories/', views.CategoryListView.as_view()),
+    path('category/<slug:slug>/detail', views.CategoryDetail.as_view()),
+    path('category-create/', views.CreateCategoryView.as_view()),
+    path('category/<slug:slug>/update/', views.UpdateCategoryView.as_view()),
+    path('category/<slug:slug>/delete/', views.DeleteCategoryView.as_view()),
+    path('category/<slug:category_slug>/<slug:group_slug>/', views.ProductListView.as_view()),
 
+    # group
+    path('category/<slug:slug>/', views.GroupListView.as_view()),
 ]
