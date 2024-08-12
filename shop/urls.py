21f24b641b81798359cb67910197fa12c25cfa15
bakhromov <najottalim@gmail.com>
2024-08-12 11:29:58 +0500

    ProductAttribute comment

--- shop/urls.py
+++ shop/urls.py
@@ -2,12 +2,13 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('categories/', views.CategoryListView.as_view()),
+    path('category/', views.CategoryListView.as_view()),
     path('category/<slug:slug>/detail', views.CategoryDetail.as_view()),
     path('category-create/', views.CreateCategoryView.as_view()),
     path('category/<slug:slug>/update/', views.UpdateCategoryView.as_view()),
     path('category/<slug:slug>/delete/', views.DeleteCategoryView.as_view()),
     path('category/<slug:category_slug>/<slug:group_slug>/', views.ProductListView.as_view()),
+    path('<slug:slug>/products/attributes/', views.ProductAttributeView.as_view(), name='product-attributes'),
 
     # group
     path('category/<slug:slug>/', views.GroupListView.as_view()),
