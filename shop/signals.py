488d323eaec4aaec58b3be3aa33b29007e88259c
bakhromov <najottalim@gmail.com>
2024-08-14 01:31:33 +0500

    rest framework auth comment

--- shop/signals.py
+++ shop/signals.py
@@ -8,9 +8,11 @@ from rest_framework.authtoken.models import Token
 def create_token(sender, instance, created, **kwargs):
     if created:
         Token.objects.create(user=instance)
+        print(f"Token created: {instance.username}")
 
 
 @receiver(post_delete, sender=User)
 def delete_token(sender, instance, **kwargs):
     instance.delete()
     Token.objects.filter(user=instance).delete()
+    print(f"Token deleted: {instance.username}")
